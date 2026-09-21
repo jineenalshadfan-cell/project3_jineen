9a7d602e979570584c1c893fbbb3646744c90bab
jineenalshadfan-cell <jineenalshadfan@gmail.com>
2026-09-21 19:56:08 +0300

    Add files via upload

---
+++ periodic_table.sql
@@ -0,0 +1,35 @@
+DELETE FROM properties WHERE atomic_number = 1000;
+DELETE FROM elements WHERE atomic_number = 1000;
+
+ALTER TABLE properties RENAME COLUMN weight TO atomic_mass;
+ALTER TABLE properties RENAME COLUMN melting_point TO melting_point_celsius;
+ALTER TABLE properties RENAME COLUMN boiling_point TO boiling_point_celsius;
+ALTER TABLE properties ALTER COLUMN melting_point_celsius SET NOT NULL;
+ALTER TABLE properties ALTER COLUMN boiling_point_celsius SET NOT NULL;
+
+ALTER TABLE elements ADD UNIQUE(symbol);
+ALTER TABLE elements ADD UNIQUE(name);
+ALTER TABLE elements ALTER COLUMN symbol SET NOT NULL;
+ALTER TABLE elements ALTER COLUMN name SET NOT NULL;
+
+ALTER TABLE properties ADD FOREIGN KEY(atomic_number) REFERENCES elements(atomic_number);
+
+CREATE TABLE types(type_id SERIAL PRIMARY KEY, type VARCHAR(20) NOT NULL);
+INSERT INTO types(type) SELECT DISTINCT type FROM properties;
+
+ALTER TABLE properties ADD COLUMN type_id INT;
+ALTER TABLE properties ADD FOREIGN KEY(type_id) REFERENCES types(type_id);
+UPDATE properties SET type_id = (SELECT type_id FROM types WHERE types.type = properties.type);
+ALTER TABLE properties ALTER COLUMN type_id SET NOT NULL;
+
+UPDATE elements SET symbol = INITCAP(symbol);
+
+ALTER TABLE properties ALTER COLUMN atomic_mass TYPE DECIMAL;
+UPDATE properties SET atomic_mass = TRIM(TRAILING '0' FROM atomic_mass::TEXT)::DECIMAL;
+
+INSERT INTO elements(atomic_number, symbol, name) VALUES (9, 'F', 'Fluorine'), (10, 'Ne', 'Neon');
+INSERT INTO properties(atomic_number, type, atomic_mass, melting_point_celsius, boiling_point_celsius, type_id) VALUES
+(9, 'nonmetal', 18.998, -220, -188.1, (SELECT type_id FROM types WHERE type = 'nonmetal')),
+(10, 'nonmetal', 20.18, -248.6, -246.1, (SELECT type_id FROM types WHERE type = 'nonmetal'));
+
+ALTER TABLE properties DROP COLUMN type;
